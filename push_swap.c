cc0ca922157b7714ea8b34930e7b1b8c4d4979b3
burak baki erdogan <bbe2007@outlook.com>
2022-08-18 18:06:59 +0300

    just doing experiments

--- push_swap.c
+++ push_swap.c
@@ -6,14 +6,35 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 13:20:45 by berdogan          #+#    #+#             */
-/*   Updated: 2022/08/18 13:22:11 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/08/18 16:25:49 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "ft_printf.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "push_swap.h"
 
 int	main(int ac, char *argv[])
 {
-	
+	int		*a;
+	int		index;
+	int		ac1;
+
+	ac1 = ac - 1;
+	index = 0;
+	a = (int *) malloc (sizeof(int) * (ac1));
+	while (ac > 1)
+	{
+		a[index] = ft_atoi(argv[index + 1]);
+		index++;
+		ac--;
+	}
+	index = 0;
+	while (ac1)
+	{
+		printf("%d\n", a[index++]);
+		ac1--;
+	}
+
 }
---
+++ push_swap.h
@@ -0,0 +1,6 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+int	ft_atoi(const char *nptr);
+int	main(int ac, char *argv[]);
+#endif
