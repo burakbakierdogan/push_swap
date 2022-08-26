5499a8ed66a17bab649a9553d7e58b8c5fd3e3b8
Burak baki Erdogan <bbe2007@outlook.com>
2022-08-26 18:36:10 +0300

    no progress

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: berdogan <berdogan@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 13:20:45 by berdogan          #+#    #+#             */
-/*   Updated: 2022/08/20 02:06:33 by berdogan         ###   ########.fr       */
+/*   Updated: 2022/08/26 18:34:44 by berdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,37 @@
 #include <stdio.h>
 #include "push_swap.h"
 
-static int	*ft_sort(int *ptr, int ac)
+#include "push_swap.h"
+
+typedef struct s_list
 {
-	int *stack_b;
-	int	index;
+	struct s_list *next;
+	int		data;
+}			t_list;
 
-	index = 0;
-	b = malloc (sizeof(char) * ac);
-	if (!b)
-		return (NULL);
-	while (1)
+/*int	ft_find_mid(t_list *begin, int ac)
+{
+	int	ac1;
+
+	ac1 = ac;
+	while (ac)
 	{
-		if (ptr[index] > ptr[index + 1])
+		while (begin)
 		{
-			res = ft_swap((ptr + index), (ptr + index + 1));
-			if(!res)
-				break;
-		}
-		else if ()
-	}
 
+		}
+	}
+}
+*/
+t_list	*ft_new_struct()
+{
+	t_list *new;
+
+	new = (t_list *) malloc (sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new -> next = NULL;
+	return (new);
 }
 
 int	main(int ac, char *argv[])
@@ -42,23 +53,25 @@ int	main(int ac, char *argv[])
 	int		*a;
 	int		index;
 	int		ac1;
+	t_list	*stack_a;
+	t_list	*hold;
 
-	ac1 = ac - 1;
-	index = 0;
-	a = (int *) malloc (sizeof(int) * (ac1));
+	index = 1;
+	stack_a = (t_list *) malloc (sizeof(t_list));
+	if (!stack_a)
+		return (0);
+	hold = stack_a;
 	while (ac > 1)
 	{
-		a[index] = ft_atoi(argv[index + 1]);
-		index++;
+		stack_a -> data = ft_atoi(*(argv + index++));
+		stack_a -> next = ft_new_struct();
+		stack_a = stack_a -> next;
 		ac--;
 	}
-	index = 0;
-
-
-	while (ac1)
+	stack_a = hold;
+	while (stack_a -> next)
 	{
-		printf("%d\n", a[index++]);
-		ac1--;
+		printf("%d\n", stack_a -> data);
+		stack_a = stack_a -> next;
 	}
-
 }
